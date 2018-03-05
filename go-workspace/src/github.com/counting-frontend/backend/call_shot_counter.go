7d420ba2960a26b13dcc0b73e28afad8be1d2e08
Mike Sallese <msallese31@gmail.com>
2018-03-04 21:09:50 -0500

    switch to calling the url of the cluster backend

--- go-workspace/src/github.com/counting-frontend/backend/call_shot_counter.go
+++ go-workspace/src/github.com/counting-frontend/backend/call_shot_counter.go
@@ -22,8 +22,6 @@ func CallShotCounter(countData data.CountObject) {
 	// bodyString := string(bodyBytes)
 	// fmt.Println(bodyString)
 
-	// TODO: CLOSE THE BODY!!!!
-
 	defer countData.Request.Body.Close()
 	err := json.NewDecoder(countData.Request.Body).Decode(&accData)
 	switch {
@@ -34,6 +32,7 @@ func CallShotCounter(countData data.CountObject) {
 		return
 	case err != nil:
 		// other error
+		fmt.Println("Unseen error decoding json: " + err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 		types.SetupAndroidResponse(w, "Bad request", 0)
 		return
@@ -47,10 +46,10 @@ func CallShotCounter(countData data.CountObject) {
 
 	fmt.Println("Calling backend")
 	// TODO: Stick configuration stuff in a context
-	backendURL := "http://localhost:5000/count"
-	req, err := http.Post(backendURL, "application/json; charset=utf-8", b)
+	backendURL := "http://shot-counter-backend:5000/count"
+	resp, err := http.Post(backendURL, "application/json; charset=utf-8", b)
 	if err != nil {
-		fmt.Println("Error talking to backend")
+		fmt.Println("Error talking to backend: " + err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		types.SetupAndroidResponse(w, "Internal server error", 0)
 		return
@@ -58,14 +57,15 @@ func CallShotCounter(countData data.CountObject) {
 
 	// TODO: Return json from backend
 	buf := new(bytes.Buffer)
-	buf.ReadFrom(req.Body)
+	buf.ReadFrom(resp.Body)
 	shotsCounted := buf.String() // Does a complete copy of the bytes in the buffer.
 	i, err := strconv.Atoi(shotsCounted)
 	if err == nil {
 		fmt.Println(i)
 	}
-	fmt.Println(accData.X)
-	fmt.Println(req)
+	fmt.Println("Shots counted from backend: " + shotsCounted)
+	fmt.Println("Shots counted from backend: " + strconv.Itoa(i))
+	fmt.Println(resp)
 	w.WriteHeader(http.StatusOK)
 	types.SetupAndroidResponse(w, "", i)
 
